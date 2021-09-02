b5ac426633a66d567c419dbc3616cd17601fbd4c
Banee Ishaque K <baneeishaque@gmail.com>
2021-09-02 19:56:24 +0530

    Pool instead of Process

--- plot-graph-via-multi-processing.py
+++ plot-graph-via-multi-processing.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from multiprocessing import Process
+import matplotlib.pyplot as matplotlib_plot
+import numpy
+import multiprocessing.pool as multi_processing_pool
 
 
 do_animation = True
@@ -9,41 +9,43 @@ do_animation = True
 def visualize_path(grid_map, start, goal, path):  # pragma: no cover
     oy, ox = start
     gy, gx = goal
-    px, py = np.transpose(np.flipud(np.fliplr(path)))
+    px, py = numpy.transpose(numpy.flipud(numpy.fliplr(path)))
     if not do_animation:
-        plt.imshow(grid_map, cmap='Greys')
-        plt.plot(ox, oy, "-xy")
-        plt.plot(px, py, "-r")
-        plt.plot(gx, gy, "-pg")
-        plt.show()
+        matplotlib_plot.imshow(grid_map, cmap='Greys')
+        matplotlib_plot.plot(ox, oy, "-xy")
+        matplotlib_plot.plot(px, py, "-r")
+        matplotlib_plot.plot(gx, gy, "-pg")
+        matplotlib_plot.show()
     else:
         for ipx, ipy in zip(px, py):
-            plt.cla()
+            matplotlib_plot.cla()
             # for stopping simulation with the esc key.
-            plt.gcf().canvas.mpl_connect(
+            matplotlib_plot.gcf().canvas.mpl_connect(
                 'key_release_event',
                 lambda event: [exit(0) if event.key == 'escape' else None])
-            plt.imshow(grid_map, cmap='Greys')
-            plt.plot(ox, oy, "-xb")
-            plt.plot(px, py, "-r")
-            plt.plot(gx, gy, "-pg")
-            plt.plot(ipx, ipy, "or")
-            plt.axis("equal")
-            plt.grid(True)
-            plt.pause(0.5)
+            matplotlib_plot.imshow(grid_map, cmap='Greys')
+            matplotlib_plot.plot(ox, oy, "-xb")
+            matplotlib_plot.plot(px, py, "-r")
+            matplotlib_plot.plot(gx, gy, "-pg")
+            matplotlib_plot.plot(ipx, ipy, "or")
+            matplotlib_plot.axis("equal")
+            matplotlib_plot.grid(True)
+            matplotlib_plot.pause(0.5)
 
 
 def main():
     start = [0, 0]
     goal = [20, 20]
-    path1 = [[0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5]]
-    path2 = [[7, 15], [8, 16], [9, 17], [10, 18], [11, 19], [12, 20]]
+    paths = [[[0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5]], [
+        [7, 15], [8, 16], [9, 17], [10, 18], [11, 19], [12, 20]]]
     grid = [[0.0 for i in range(20)] for j in range(20)]
     print(grid)
-    print(path1)
-    print(path2)
-    Process(target=visualize_path(grid, start, goal, path1)).start()
-    Process(target=visualize_path(grid, start, goal, path2)).start()
+    print(paths[0])
+    print(paths[1])
+    local_multi_processing_pool = multi_processing_pool.Pool()
+    [local_multi_processing_pool.apply(func=visualize_path, args=(
+        grid, start, goal, path)) for path in paths]
+    local_multi_processing_pool.close()
 
 
 if __name__ == "__main__":
